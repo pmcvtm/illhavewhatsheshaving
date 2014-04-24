7f68bbd0466cbc91b8671353e9a1b80d77b3b21b
Patrick McVeety-Mill <p.mcvtm@gmail.com>
2014-04-24 01:44:12 -0500

    added test showing true 'mock' using RhinoMocks

--- src/Core.Tests/Baking/StubbedCakeMakerTester.cs
+++ src/Core.Tests/Baking/StubbedCakeMakerTester.cs
@@ -1,5 +1,7 @@
 ﻿using Core.Baking;
+using Core.Baking.Services;
 using NUnit.Framework;
+using Rhino.Mocks;
 using Should;
 
 namespace Core.Tests.Baking
@@ -18,6 +20,24 @@ namespace Core.Tests.Baking
 
 			cake.HasBeenBaked.ShouldBeTrue();
 			cake.HasBeenDecordated.ShouldBeTrue();
+		}
+
+		[Test]
+		public void Should_bake_at_350_for_30_minutes()
+		{
+			var recipe = new TestCakeRecipe();
+
+			var oven = MockRepository.GenerateMock<OvenService>();
+			
+			var mixer = MockRepository.GenerateStub<IMixerService>();
+			mixer.Stub(x => x.Mix(null)).IgnoreArguments().Return(true);
+
+			new CakeMaker(oven, mixer).MakeACake(recipe);
+
+			var args = oven.GetArgumentsForCallsMadeOn(x => x.Bake(0, 0), opt => opt.IgnoreArguments());
+			
+			args[0][0].ShouldEqual(350);
+			args[0][1].ShouldEqual(30);
 		}
     }
 }
