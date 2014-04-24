0db8aaba2fd78d4d3e7469c0ad8d63d5f4a7b3fe
Patrick McVeety-Mill <p.mcvtm@gmail.com>
2014-04-24 01:04:14 -0500

    added simple unit test that covers whole process

---
+++ src/Core.Tests/Baking/CakeMakerTester.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Core.Baking;
+using Core.Baking.Services;
+using NUnit.Framework;
+using Should;
+
+namespace Core.Tests.Baking
+{
+	[TestFixture]
+    public class CakeMakerTester
+    {
+		[Test]
+		public void Should_bake_and_decorate_cake()
+		{
+			var recipe = new TestCakeRecipe();
+
+			var cakeMaker = new CakeMaker(new ElectricOvenService(), new WoodenSpoonService());
+
+			var cake = cakeMaker.MakeACake(recipe);
+
+			cake.HasBeenBaked.ShouldBeTrue();
+			cake.HasBeenDecordated.ShouldBeTrue();
+		}
+    }
+
+	public class TestCakeRecipe : ICakeDto
+	{
+		public string Name { get { return "Test Cake"; } }
+		public List<Ingredient> Ingredients { get {return new List<Ingredient>();} }
+	}
+}
